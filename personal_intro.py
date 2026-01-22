cbdd9e2969f780f38e5ced12c7754902e8ac6cb1
Sneha Harshitha Kankatala <snehaharshithakankatala@gmail.com>
2026-01-22 19:39:15 +0530

    Add personal_intro.py

---
+++ personal_intro.py
@@ -0,0 +1,7 @@
+name=str(input("enter your name:"+" "))
+age=int(input("enter your age:"+""))
+hobby=str(input("enter your hobbys:"+""))
+print("I'm really glad you're here"+"")
+print("welcome"+name+" ")
+print("you are"+" "+ str(age) +" "+"old"+" "+"you love"+" "+hobby)
+
